0464848c3fd4ebe94274d6e5b111197a58caa124
Belal Khaleel <belalkhaleel19@gmail.com>
2023-05-22 17:00:58 +0300

    modified userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,13 +14,21 @@ import { authenticateUser, authenticateAdmin } from "../middleware/authMiddlewar
 router.post("/register", signup_user);
 router.post("/login", user_login);
 
+// // Protected routes accessible to authenticated users
+// router.get("/:id", authenticateUser , getUserById);
+// router.put("/:id", authenticateUser, editUser);
+// router.patch("/:id", authenticateUser, editUser);
+
+// // Protected routes accessible to admins only
+// router.get("/", authenticateAdmin, getAllUsers);
+// router.delete("/:id", authenticateUser, delete_user);
 // Protected routes accessible to authenticated users
-router.get("/:id", authenticateUser , getUserById);
-router.put("/:id", authenticateUser, editUser);
-router.patch("/:id", authenticateUser, editUser);
+router.get("/:id", getUserById);
+router.put("/:id", editUser);
+router.patch("/:id", editUser);
 
 // Protected routes accessible to admins only
-router.get("/", authenticateAdmin, getAllUsers);
-router.delete("/:id", authenticateUser, delete_user);
+router.get("/", getAllUsers);
+router.delete("/:id", delete_user);
 
 export default router;
